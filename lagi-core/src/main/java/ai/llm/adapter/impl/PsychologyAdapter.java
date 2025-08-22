2d1b11fc3622dceb6825ba386e94e8a3b68cfa6f
Shawn <xiuwen.zhao@landingbj.com>
2025-08-22 16:18:49 +0800

    Create PsychologyAdapter.java

---
+++ lagi-core/src/main/java/ai/llm/adapter/impl/PsychologyAdapter.java
@@ -0,0 +1,35 @@
+package ai.llm.adapter.impl;
+
+import ai.openai.pojo.ChatCompletionRequest;
+import ai.openai.pojo.ChatCompletionResult;
+import io.reactivex.Observable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PsychologyAdapter extends OpenAIStandardAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(PsychologyAdapter.class);
+
+    @Override
+    public String getApiAddress() {
+        if (apiAddress == null) {
+            apiAddress = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions";
+        }
+        return apiAddress;
+    }
+
+    @Override
+    public ChatCompletionResult completions(ChatCompletionRequest chatCompletionRequest) {
+        setReplaceModel(chatCompletionRequest);
+        return super.completions(chatCompletionRequest);
+    }
+
+    @Override
+    public Observable<ChatCompletionResult> streamCompletions(ChatCompletionRequest chatCompletionRequest) {
+        setReplaceModel(chatCompletionRequest);
+        return super.streamCompletions(chatCompletionRequest);
+    }
+
+    private void setReplaceModel(ChatCompletionRequest chatCompletionRequest) {
+        chatCompletionRequest.setModel("qwen-turbo");
+    }
+}

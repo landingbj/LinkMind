f3dc35bdd10c5a35c45ee9cffabde217e0cf7b49
Shawn <xiuwen.zhao@landingbj.com>
2025-09-17 14:27:49 +0800

    fix bugs

--- lagi-web/src/main/java/ai/servlet/api/LlmApiServlet.java
+++ lagi-web/src/main/java/ai/servlet/api/LlmApiServlet.java
@@ -33,6 +33,7 @@ import ai.medusa.pojo.PromptInput;
 import ai.medusa.MedusaMonitor;
 import ai.medusa.utils.PromptCacheTrigger;
 import ai.medusa.utils.PromptInputUtil;
+import ai.openai.pojo.ChatCompletionChoice;
 import ai.router.pojo.LLmRequest;
 import ai.servlet.BaseServlet;
 import ai.utils.ClientIpAddressUtil;
@@ -376,7 +377,10 @@ public class LlmApiServlet extends BaseServlet {
 
     private ChatCompletionRequest setCustomerModel(HttpServletRequest req, HttpSession session) throws IOException {
         ModelPreferenceDto preference = JSONUtil.toBean((String) session.getAttribute("preference"), ModelPreferenceDto.class) ;
-        ChatCompletionRequest chatCompletionRequest = objectMapper.readValue(requestToJson(req), ChatCompletionRequest.class);
+        String json = requestToJson(req);
+        logger.info(json);
+
+        ChatCompletionRequest chatCompletionRequest = objectMapper.readValue(json, ChatCompletionRequest.class);
         if(chatCompletionRequest.getModel() == null
                 && preference != null
                 && preference.getLlm() != null) {
@@ -409,9 +413,20 @@ public class LlmApiServlet extends BaseServlet {
                 data -> {
                     lastResult[0] = data;
                     ChatCompletionResult filter = SensitiveWordUtil.filter(data, true);
-                    String msg = gson.toJson(filter);
-                    out.print("data: " + msg + "\n\n");
-                    out.flush();
+                    if (filter != null && filter.getChoices() != null && !filter.getChoices().isEmpty() && filter.getChoices().get(0).getFinish_reason() != null ) {
+                        ChatCompletionChoice chatCompletionChoice = filter.getChoices().get(0);
+                        String finishReason = chatCompletionChoice.getFinish_reason();
+                        chatCompletionChoice.setFinish_reason(null);
+                        String msg = gson.toJson(filter);
+                        outputChunk(out, msg);
+                        chatCompletionChoice.setFinish_reason(finishReason);
+                        chatCompletionChoice.getDelta().setContent("");
+                        msg = gson.toJson(filter);
+                        outputChunk(out, msg);
+                    } else {
+                        String msg = gson.toJson(filter);
+                        outputChunk(out, msg);
+                    }
                     if (medusaMonitor != null && promptInput != null && filter != null) {
                         medusaMonitor.put(key, new CacheItem(promptInput, filter));
                     }
@@ -433,6 +448,12 @@ public class LlmApiServlet extends BaseServlet {
                     }
                 }
         );
+    }
+
+    private void outputChunk(PrintWriter out, String msg) {
+        logger.info("data: {}", msg);
+        out.print("data: " + msg + "\n\n");
+        out.flush();
     }
 
     private void extracted(ChatCompletionResult[] lastResult, List<IndexSearchData> indexSearchDataList, GetRagContext ragContext, PrintWriter out) {
